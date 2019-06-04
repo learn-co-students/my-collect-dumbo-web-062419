41ec71b59319e6d9abbd233d7e848d55b387201e
github username <github email address>
2019-06-04 12:02:59 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(collection)
+    i = 0
+    value = []
 
+    while i < collection.length 
+        value << yield(collection[i])
+        i += 1
+    end
+    value
+end
 
+my_collect(collection) do |name|
+    name.split(" ").first
+    
+end

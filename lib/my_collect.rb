4653ad24668717e64d869180063a72590f8377fa
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 00:34:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 
+def my_collect(array)
+  i=0
+  newArray = []
+  while i < array.length
+  newArray << yield(array[i])
+  i += 1
+end
+
+  newArray
+end
+
+
+my_collect(array) do |x|
+  x.split(" ").first
+end
+
+
+my_collect(collection) do |y|
+  y.upcase
+end

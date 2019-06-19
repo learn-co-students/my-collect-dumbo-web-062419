5f25ee3cca36c81f14d5d6472e59f4e2458f17ef
Aliciea D Suechin <adr330@nyu.edu>
2019-06-19 18:28:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,24 @@
 
-def my_collect
-empty_array = []
-counter = 0
-my_collect(empty_array) do |counter|
-  counter += 1 
+def my_collect(empty_array)
+  empty_array.each do |x|
+    x += 1 
+  end 
 end 
+
+def my_collect(languages)
+  languages.each do |lang|
+    lang.upcase 
+end
+end
+
+def my_collect(languages)
+  languages.each do |lang|
+    lang.upcase 
+  end
+  languages
+end 
+
+my_collect(names) 
+names.collect do |name|
+  name.split(" ").first 
 end 
